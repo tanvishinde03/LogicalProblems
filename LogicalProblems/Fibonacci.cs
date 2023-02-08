7e86c584d7a065601142c2c1a1c5ab914829b63d
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-08 09:40:30 +0530

    [Tanvi] Add : UC1 - Fibonacci Number

---
+++ LogicalProblems/Fibonacci.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class Fibonacci
+    {
+
+        public void Displayfibonacci()
+        {
+            int n1 = 0, n2 = 1, n3, i, number;
+            Console.WriteLine("Enter the number of elements");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1 + " " + n2 + " ");
+
+            for (i = 2; i < number; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    
+
---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Fibonacci Series");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    
+                    Fibonacci fibonacci = new Fibonacci();
+                    fibonacci.Displayfibonacci();
+                    break;
+
+
+            }
+            Console.ReadLine();
+        }
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    
+
